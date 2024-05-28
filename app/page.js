4dd7fa9d2352f3a98edd50ad2611ff3a91bdb1bc
Sabaiduka14 <sabaidukashvili50@gmail.com>
2024-05-28 22:41:35 +0400

    new files and folders

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Hero } from "@/components/components/hero";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Hero />
+    </div>
+  );
+}
